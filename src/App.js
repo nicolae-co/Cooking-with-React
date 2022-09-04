719992566f18b4bc9589cd79f96053202e1765f9
nicolae-co <cojocarugithub@gmail.com>
2022-09-04 13:39:28 +0300

    Cooknig app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React,{useState} from "react";
-import Counter from "./Counter"
-import CounterHooks from "./CounterHooks";
+import React from "react";
+import RecipeList from "./RecipeList";
+
 
 export const ThemeContext = React.createContext()
 
 function App() {
-  const [theme, setTheme] = useState("green")
   return (
-    <ThemeContext.Provider value={{backgroundColor: theme}}>
-        Counter
-        <Counter initialCount={0} />
-        Counter Hooks
-        <CounterHooks initialCount={0} />
-        <button onClick={()=> setTheme(prevTheme => {
-          return prevTheme === 'red' ? 'blue' : 'red'
-        })}>Toggle Theme</button>
-     </ThemeContext.Provider>
-    )
+    <RecipeList recipes = {sampleRecipes}/>
+  );
 }
+
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: "Plain Chicken",
+    servings: 3,
+    cookTime: '1:45',
+    instructions: "1. Put salt on chicken\n 2. Put chicken in oven\n 3. Eat the chicken"
+  },
+  {
+    id: 2,
+    name: "Plain Pork",
+    servings: 5,
+    cookTime: '0:45',
+    instructions: "1. Put paprika on pork\n 2. Put pork in oven\n 3. Eat the pork"
+  }
+]
 
 export default App;
